0e02aee7c2c0121dba0f6d7c0b0f4fccfc26040e
`Z`A`C`K` <58990407+zack-opp47@users.noreply.github.com>
2021-03-18 22:58:30 +0700

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 SESSION_NAME = getenv("1BVtsOK0Bu4mERJs2VqCsyawjPo-t0b2Iuri-ly95PdxMoU7LQNjGSYXyv7yr83VSf2vTmE7T6YzzVeVGzmpAnM5WctTb3L1q3AKtktLhhXqfRVkVq3noyKiL5jMNB27sv2mV_YmhlVShtO2dTAeTRSAvem8G5wYQgAimswVOIUa7jXsaE87tzQkt3p6KawM0q37b-2h41r9hi7H_2TNHzpoLt6U7dCGIhdjrTdzzUrucX8x2j0dsKH_THvlsCWRKzsvyOEiU21qJ6d9daol_7PqQ1zvQBV_B4nRGa6J3xXtJZQMaG34im34sopKD0rVmvOAZk1PZW9-BIcV7AV04OMq3rFvrnUU=", "session")
 BOT_TOKEN = getenv("1781912435:AAFPy9aWmUySA7b_b9XOgj2MrxoApryk5bM")
 
-API_ID = int(getenv("3337619")
+API_ID = getenv("3337619")
 API_HASH = getenv("e5fc2bd9e8f028e4d67694694d2612f7")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
